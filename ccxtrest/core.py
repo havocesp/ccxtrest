02e695ad8e1ff90840c42fccf4a346661a8a254c
Daniel J. Umpierrez <umpierrez@pm.me>
2018-12-12 14:30:14 +0100

    - Added Exchang APIs container class - removed pandaxt dependence

--- ccxtrest/core.py
+++ ccxtrest/core.py
@@ -7,64 +7,69 @@
   - Created:    13-11-2018
   - Modified:   13-11-2018
 """
-import typing as tp
+from collections import UserDict
 
-import pandaxt
+import ccxt
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
-exchange_list = ['hitbtc2', 'cryptopia', 'binance']
-exchanges = dict.fromkeys(exchange_list)  # type: tp.Dict[tp.AnyStr, pandaxt.PandaXT]
-
-for ex in exchange_list:
-    exchanges[ex] = pandaxt.PandaXT(ex)
 
 
-# def do_info_drop(data, symbol=None, **kwargs):
-#     if symbol and symbol in data:
-#         data = data.get(symbol or '', data)
-#         if 'info' in data:
-#             del data['info']
-#     elif isinstance(data, dict):
-#         data = {k: {x: y for x, y in v.items() if y and x not in ['info']} for k, v in data.items()}
-#     elif isinstance(data, (list, tuple, set)):
-#         data = [{x: y for x, y in v.items() if y and x not in ['info']} if isinstance(v, dict) else v for v in data]
-#     return data
+class Exchanges(UserDict):
+
+    def __init__(self, *exchanges):
+        settings = {'timeout': 15000}
+
+        super().__init__(**{e: getattr(ccxt, e)(settings) for e in exchanges if e in ccxt.exchanges})
+
+    def __getattr__(self, item):
+        return self.data[item]
+
+
+selected = dict(binance='binance', crytopia='cryptopia', hitbtc2='hitbtc2')
+
+API = Exchanges(*selected)
 
 
 @app.route('/ticker/<exchange>/', methods=['GET'])
 def get_ticker(exchange):
     symbol = str(request.args.get('symbol')).replace('_', '/')
-    data = exchanges[exchange].get_tickers(symbol)
-    return jsonify(data.data)
+    data = API[exchange].fetch_tickers(symbol)
+    return jsonify(data)
 
 
 @app.route('/ohlc/<exchange>/', methods=['GET'])
 def get_ohlcv(exchange):
     args = dict(limit=25, timeframe='15m')
     args.update(symbol=request.args.get('symbol'), **args)
-    data = exchanges[exchange].get_ohlc(**args)
+    data = API[exchange].fetch_ohlcv(**args)
     return jsonify(data.to_dict().values())
 
 
-# noinspection PyUnresolvedReferences
 @app.route('/book/<exchange>/', methods=['GET'])
 def get_orderbook(exchange):
-    args = dict(limit=5)
-    args.update(**request.args)
-    data = get_exchange(exchange).fetch_order_book(**args)
-    return jsonify(do_info_drop(data, **args))
+    limit = request.args.get('limit', 5)
+    symbol = str(request.args.get('symbol')).replace('_', '/')
+
+    data = API[exchange].fetch_order_book(symbol=symbol, limit=limit)
+    if data is None or not isinstance(data, (list, dict)):
+        raise TypeError('Invalid {} type.'.format(str(data)))
+    elif isinstance(data, dict):
+        data = [data]
+    else:
+        data = [{k: v for k, v in d.items() if k not in 'info'} for d in data]
+    return jsonify(data)
 
 
-# @app.route('/markets/<exchange>', defaults={'symbol1': None, 'symbol2': None}, methods=['GET'])
 @app.route('/market/<exchange>/', methods=['GET'])
 def get_markets(exchange):
     symbol = str(request.args.get('symbol')).replace('_', '/')
-    markets = exchanges[exchange].markets
+    markets = API[exchange].markets
     return jsonify(markets.get(symbol))
 
+    # now exchanges dictionary contains all exchange instances...
 
-# now exchanges dictionary contains all exchange instances...
+
 if __name__ == '__main__':
     app.run()
